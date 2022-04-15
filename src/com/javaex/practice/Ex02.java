e3930dbeaa3ff7a8b8fa5f7200f5edb537b0b6ab
y <y@DESKTOP-EDBR2CG>
2022-04-15 18:17:20 +0900

    2022-04-15 practice02

---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,22 @@
+package com.javaex.practice;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		int x = 0;
+		//x가 0과 같으면
+		
+		/*
+		if(x=0) {
+			System.out.println("x는 0이다.");
+		}
+		*/
+		// x=0 을 x==0으로 수정한다.
+		
+		
+		if(x==0) {
+			System.out.println("x는 0이다.");
+		}
+	}
+
+}
---
+++ src/com/javaex/practice/Ex03.java
@@ -0,0 +1,35 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		//결과---> 큰따옴표(\"), 작은따옴표(\')
+		//15 : '2번그룹'\n입니다.
+		//19 : '2번그룹'\n입니다.
+		//20 : '2번그룹'\n입니다.
+		//21 : "1번그룹"\n입니다.
+		//100 : "1번그룹"\n입니다.
+		
+		
+		int age;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("나이를 입력해주세요");
+		System.out.print("나이: ");
+		age = sc.nextInt();
+		
+		if(age>20) {
+			System.out.println("\"1번그룹\"");
+		} else {
+			System.out.println("\'2번그룹\'");
+		}
+		System.out.println("입니다.");
+	
+		
+		sc.close();
+
+	}
+
+}
